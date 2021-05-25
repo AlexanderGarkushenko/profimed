f1219a7559082cb6a42a27041c40c6877a546daa
mewmewdes <geka3506@gmail.com>
2021-05-25 10:36:06 +0300

    init

---
+++ index.php
@@ -0,0 +1,351 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bookishow</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <div class="parent">
+        <div class="divfirpanel">
+            <div class="firstpanel">
+                <div class="firsthalf">
+                    <div class="location">Локация: 121 Wallstreet Street, NY , USA</div>
+                    <div class="email">Почта:<a class="mail" href="../other/email.html">info@profimed.com</a></div>
+                </div>
+                <div class="secondhalf">
+                    <div class="number">Экстренный вызов: <a class="numb" href="other/number.html">800 123 45 67</a>
+                    </div>
+                    <div class="links">
+                        <i class="fa fa-linkedin soc"></i>
+                        <i class="fa fa-twitter soc"></i>
+                        <i class="fa fa-facebook soc"></i>
+                        <i class="fa fa-instagram soc"></i>
+                    </div>
+                    <div class="loop">
+                        <i class="fa fa-search"></i>
+                    </div>
+                    <div class="cart">
+                        <i class="fa fa-cart-plus"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="divsecpanel">
+            <div class="secondpanel">
+                <a href="index.php" class="logo"><img src="image/logo.png"></a>
+                <a href="layout/home.php" class="list">Домашняя</a>
+                <a href="layout/departaments.php" class="list">Отделения</a>
+                <a href="layout/doctors.php" class="list">Доктора</a>
+                <a href="layout/timetable.php" class="list">Расписание</a>
+                <a href="layout/news.php" class="list">Новости</a>
+                <a href="layout/shop.php" class="list">Магазин</a>
+                <a href="index.php" class="buttonsub">
+                    <button class="subscribe">Записаться на прием</button>
+                </a>
+            </div>
+        </div>
+        <div class="title">
+            <p class="health">Несем здоровье</p>
+            <p class="life">и жизнь всей семье</p>
+            <div class="viewdiv">
+                <button class="view">Просмотр отделений</button>
+            </div>
+        </div>
+    </div>
+    <!-- Конец шапки -->
+    <div class="backcolor1">
+        <section class="onesect">
+            <div class="ophours">
+                <h3 class="headline">Часы работы</h3><br>
+                <div class="wday1">
+                    <p class="weekday">Понедельник-Пятница</p>
+                    <span class="worktime">8.00-18.00</span>
+                </div>
+                <div class="wday2">
+                    <p class="weekday">Суббота</p>
+                    <span class="worktime">9.00-17.00</span>
+                </div>
+                <div class="wday3">
+                    <p class="weekday">Воскресенье</p>
+                    <span class="worktime">9.00-15.00</span>
+                </div>
+            </div>
+            <div class="doctorstime">
+                <h3 class="headline">Расписание врачей</h3><br>
+                <p class="plan">Ваш план лечения рассчитан на долговременный прогресс, причем каждая фаза будет
+                    реализована как можно быстрее.</p><br>
+                <a href="layout/timetable.php" class="divviewtable">
+                    <button class="viewtable"><i class="fa fa-calendar-o"></i>Просмотр расписания</button>
+                </a>
+            </div>
+            <div class="emcases">
+                <h3 class="headline">Чрезвычайные ситуации</h3><br>
+                <div class="numbertwo">
+                    <div class="phone">
+                        <i class="fa fa-phone"></i>
+                    </div>
+                    <h2 class="numbtwo">800 123 45 67</h2>
+                </div><br>
+                <p class="plan">Ваш план лечения рассчитан на долговременный прогресс, причем каждая фаза будет
+                    реализована как можно быстрее.</p>
+            </div>
+        </section>
+    </div>
+    <!--Конец панели-->
+    <section class="backg">
+        <div class="divservname">
+            <h3 class="servname">Сервисы</h3>
+            <span class="servbord"></span>
+        </div>
+        <section class="services">
+            <div class="container5">
+                <img src="image/doctor.webp">
+                <h5 class="name5">Опытные врачи</h5>
+                <p class="text5">Ваше здоровье - это ваш самый важный актив. Вы должны доверить его только лучшим
+                    профессионалам.</p>
+            </div>
+            <div class="container5">
+                <img src="image/heart.webp">
+                <h5 class="name5">Индивидуальный Подход</h5>
+                <p class="text5">Выбор лечения идеально соответствует вашим целям лечения осложнений при раннем
+                    вмешательстве.</p>
+            </div>
+            <div class="container5">
+                <img src="image/shield.webp">
+                <h5 class="name5">Качество и безопасность</h5>
+                <p class="text5">Все члены команды Медицинского центра прошли тщательную подготовку, чтобы помочь в
+                    любой ситуации.</p>
+            </div>
+            <div class="container5">
+                <img src="image/med.webp">
+                <h5 class="name5">Быстрый сервис</h5>
+                <p class="text5">Ваш план лечения рассчитан на долговременный прогресс, причем каждая фаза выполняется
+                    как можно быстрее.</p>
+            </div>
+        </section>
+    </section>
+    <!-- Конец панели -->
+    <div class="backim">
+        <section class="backg2">
+            <div>
+                <img class="litllogo" src="image/logo-small.webp">
+            </div>
+            <h2 class="welcome">Добро пожаловать в нашу клинику!</h2>
+            <h4 class="medproc">Мы предлагаем широкий спектр медицинских услуг для уходящих и приходящих пациентов.</h4>
+            <p class="special">Наши основные направления специализации включают онкологию, ортопедию, кардиологию,
+                лечение ЭКО, урологию,
+                нейрохирургию, гастроэнтерологию, пластическую хирургию и многие другие отделения.</p>
+            <div class="divgetin">
+                <button class="getin">Связаться</button>
+            </div>
+        </section>
+    </div>
+    <!-- Конец панели -->
+    <div class="backg3">
+        <div class="servnamediv">
+            <h3 class="servname1">Отделения</h3>
+            <span class="yu"></span>
+        </div>
+        <section class="conticons1">
+            <div class="icons">
+                <img src="image/icon-dep-1.webp">
+                <a href="./layout/neurology.php" class="names">Неврология</a>
+                <p class="relaxtext">Лечение идеально соответствует вашим целям</p>
+            </div>
+            <div class="icons">
+                <img src="image/icon-dep-2.webp">
+                <a class="names">Травматология</a>
+                <p class="relaxtext">Лечение идеально соответствует вашим целям</p>
+            </div>
+            <div class="icons">
+                <img src="image/icon-dep-3.webp">
+                <a class="names">Нефрология</a>
+                <p class="relaxtext">Лечение идеально соответствует вашим целям</p>
+            </div>
+            <div class="icons">
+                <img src="image/icon-dep-4.webp">
+                <a class="names">Кардиология</a>
+                <p class="relaxtext">Лечение идеально соответствует вашим целям</p>
+            </div>
+            <div class="icons">
+                <img src="image/icon-dep-5.webp">
+                <a class="names">Пульмонология</a>
+                <p class="relaxtext">Лечение идеально соответствует вашим целям</p>
+            </div>
+            <div class="icons">
+                <img src="image/icon-dep-6.webp">
+                <a class="names">Деторождение</a>
+                <p class="relaxtext">Лечение идеально соответствует вашим целям</p>
+            </div>
+        </section>
+        <section class="conticons2">
+            <div class="icons">
+                <img src="image/icon-dep-7.webp">
+                <a class="names">Гинекология</a>
+                <p class="relaxtext">Лечение идеально соответствует вашим целям</p>
+            </div>
+            <div class="icons">
+                <img src="image/icon-dep-8.webp">
+                <a class="names">Стамотология</a>
+                <p class="relaxtext">Лечение идеально соответствует вашим целям</p>
+            </div>
+            <div class="icons">
+                <img src="image/icon-dep-9.webp"><br>
+                <a class="names">Рентген</a><br>
+                <p class="relaxtext">Лечение идеально соответствует вашим целям</p>
+            </div>
+            <div class="icons">
+                <img src="image/icon-dep-10.webp">
+                <a class="names">Беременность</a>
+                <p class="relaxtext">Лечение идеально соответствует вашим целям</p>
+            </div>
+            <div class="icons">
+                <img src="image/icon-dep-11.webp">
+                <a class="names">Инвалидность</a>
+                <p class="relaxtext">Лечение идеально соответствует вашим целям</p>
+            </div>
+            <div class="icons">
+                <img src="image/icon-dep-12.webp"><br>
+                <a class="names">ЯМР</a><br>
+                <p class="relaxtext">Лечение идеально соответствует вашим целям</p>
+            </div>
+        </section>
+    </div>
+    <!-- Конец панели -->
+    <div class="backg4">
+        <div class="hallway">
+            <button class="leftbut"><img src="image/left.webp" class="arowl"></button>
+            <button class="rightbut"><img src="image/right.webp" class="arowr"></button>
+        </div>
+    </div>
+    <!-- Конец панели -->
+    <div class="sizespec">
+        <div class="medspecdiv">
+            <h3 class="medspec">Медицинские специалисты</h3>
+            <span class="underscore"></span>
+        </div><br>
+        <div class="team">
+            <div class="teamdiv">
+                <div class="photodiv"><img class="phototeam" src="image/team-1.jpg"></div>
+                <div class="descripteam">
+                    <span class="profmed">Доктор-консультант</span><br>
+                    <div class="nameteamdiv">
+                        <a class="nameteam" href="other/names.html">Др. Сара Джонс</a><br>
+                    </div>
+                    <span class="teamnumb"><i class="fa fa-tablet pr6"></i>+ 800 123 45 67</span><br>
+                    <a class="emaillink" href="other/email.html"><i
+                            class="fa fa-envelope pr6"></i>info@profimed.com</a><br>
+                </div>
+            </div>
+            <div class="teamdiv">
+                <div class="photodiv"><img class="phototeam" src="image/team-2.jpg"></div>
+                <div class="descripteam">
+                    <span class="profmed">Стоматолог-гигиенист</span><br>
+                    <div class="nameteamdiv">
+                        <a class="nameteam" href="other/names.html">Др. Грег Хаус</a><br>
+                    </div>
+                    <span class="teamnumb"><i class="fa fa-map-marker pr6"></i>Медицинский доктор</span><br>
+                    <a class="emaillink" href="other/email.html"><i
+                            class="fa fa-envelope pr6"></i>info@profimed.com</a><br>
+                </div>
+            </div>
+            <div class="teamdiv">
+                <div class="photodiv"><img class="phototeam" src="image/team-3.jpg"></div>
+                <div class="descripteam">
+                    <span class="profmed">Педиатр</span><br>
+                    <div class="nameteamdiv">
+                        <a class="nameteam" href="other/names.html">Др. Марта Стюарт</a><br>
+                    </div>
+                    <span class="teamnumb"><i class="fa fa-tablet pr6"></i>+ 800 123 45 67</span><br>
+                    <a class="emaillink" href="other/email.html"><i
+                            class="fa fa-envelope pr6"></i>info@profimed.com</a><br>
+                </div>
+            </div>
+            <div class="teamdiv">
+                <div class="photodiv"><img class="phototeam" src="image/team-4.jpg"></div>
+                <div class="descripteam">
+                    <span class="profmed">Кардиохирург</span><br>
+                    <div class="nameteamdiv">
+                        <a class="nameteam" href="other/names.html">Др. Найт Шьямалан</a><br>
+                    </div>
+                    <span class="teamnumb"><i class="fa fa-tablet pr6"></i>+ 800 123 45 67</span><br>
+                    <a class="emaillink" href="other/email.html"><i
+                            class="fa fa-envelope pr6"></i>info@profimed.com</a><br>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Конец панели -->
+    <section class="makeappsec">
+        <div class="backg5">
+            <div class="pos1">
+                <div class="rero">
+                    <h4 class="makeapp">Записаться на прием</h4>
+                    <span class="underscore2"></span>
+                    <details>
+                        <summary class="seldep rigin">Выбрать отделение</summary>
+                        <p>Неврология</p>
+                        <p>Травмотология</p>
+                        <p>Нефрология</p>
+                        <p>Кардиология</p>
+                        <p>Пульмонология</p>
+                        <p>Деторождение</p>
+                    </details>
+                    <p class="fullnamep rigin">
+                        <input class="fullname" type="text" placeholder="Полное имя">
+                    </p>
+                    <p class="fullnamep rigin">
+                        <input class="fullname" type="text" placeholder="Номер телефона">
+                    </p>
+                    <p class="calendarp rigin">
+                        <span>
+                            <input class="calendar" type="date" placeholder="дд.мм.ггг">
+                        </span>
+                        <i class="fa fa-calendar-o cal-r"></i>
+                    </p>
+                    <p>
+                        <input class="sendre" type="button" value="Отправить запрос">
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="backg6">
+            <div class="pos2">
+                <h4 class="makeapp">Последние новости</h4>
+                <span class="underscore3"></span>
+                <div class="news">
+                    <time datetime="2016-07-12T06:07:04+00:00" title="2016-07-12T06:07:04+00:00">
+                        <i class="fa fa-calendar-o cal-1" aria-hidden="true"></i>Jul 12, 2016</time>
+                    <a class="frnew" href="other/">Трудности обучения диагностическому мышлению в цифровую эпоху:
+                        Критическая роль
+                        учителя-наставника-клинициста</a>
+                    <div>
+                        <span>создано <a class="by-in" href="other/admin.html">admin</a></span>
+                        <span>по <a class="by-in" href="other/theme.html">theme</a></span>
+                    </div>
+                </div>
+                <div class="news bordnew">
+                    <time datetime="2016-07-12T06:03:02+00:00" title="2016-07-12T06:03:02+00:00">
+                        <i class="fa fa-calendar-o cal-1" aria-hidden="true"></i>Jul 12, 2016</time>
+                    <a class="frnew" href="other/">Два бронходилататора длительного действия в одном дозированном
+                        ингаляторе</a>
+                    <div>
+                        <span>создано <a class="by-in" href="other/admin.html">admin</a></span>
+                        <span>по <a class="by-in" href="other/theme.html">theme</a></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Конец панели -->
+<?php require_once("./layout/footer.php")?>
+</body>
+
+</html>
---
+++ layout/footer.php
@@ -0,0 +1,82 @@
+<section class="clients">
+    <div class="cli-im"></div>
+    <div class="cli-im"></div>
+    <div class="cli-im"></div>
+    <div class="cli-im"></div>
+</section>
+<footer>
+    <div class="backg7 bor">
+        <div class="content">
+            <div class="ent-email">
+                <input class="inp-email" type="email" placeholder="Введите эл. почту">
+                <div class="div-fafa">
+                    <a><i class="fa fa-envelope-o"></i></a>
+                </div>
+            </div>
+            <div class="soc-web">
+                <i class="fa fa-linkedin soc"></i>
+                <i class="fa fa-twitter soc"></i>
+                <i class="fa fa-facebook soc"></i>
+                <i class="fa fa-instagram soc"></i>
+            </div>
+        </div>
+    </div>
+    <div class="backg7">
+        <div class="footer">
+            <div class="pillars Santana">
+                <img class="inm" src="../image/logo.png">
+                <p>Выбор правильной больницы и врача-это важные факторы, которые необходимо учитывать
+                    и которые существенно влияют на лечение пациента.
+                    Предпочтительным выбором для многих пациентов является выбор частного ухода.</p>
+            </div>
+            <div class="pillars Cars">
+                <div>
+                    <h6 class="big-zag">Отделения</h6>
+                    <span class="underscore4"></span>
+                </div>
+                <a class="foot-dep">Неврология</a>
+                <a class="foot-dep">Травматология</a>
+                <a class="foot-dep">Гинекология</a>
+                <a class="foot-dep">Нефрология</a>
+                <a class="foot-dep">Кардиология</a>
+                <a class="foot-dep">Пульмонология</a>
+            </div>>
+            <div class="pillars">
+                <div>
+                    <h6 class="big-zag">Главный офис</h6>
+                    <span class="underscore4"></span>
+                </div>
+                <p class="foot-dep">4096 Хайленд-ст, Арлингтон<br>
+                    Верджиния 32101, США<br>
+                    info@profimed.com<br>
+                    800 1234 56 78<br>
+                </p>
+                <p class="foot-dep other">
+                    Пн-чт: 09:30-21:00<br>
+                    Пт: 06:00-21:00<br>
+                    Сб: 10:00-15:00<br>
+                </p>
+            </div>
+            <div class="pillars">
+                <div>
+                    <h6 class="big-zag">Быстрые ссылки</h6>
+                    <span class="underscore4"></span>
+                </div>
+                <a class="foot-q">Домашняя</a>
+                <a class="foot-q">Отделения</a>
+                <a class="foot-q">На тему...</a>
+                <a class="foot-q">Новости</a>
+                <a class="foot-q">Контакты</a>
+            </div>
+        </div>
+    </div>
+    <div class="backg8">
+        <div class="deep-fo">
+            <div>
+                <a class="ter-co">Условия и положения</a>
+                <a class="priv-po">Политика конфиденциальности</a>
+            </div>
+            <p>© 2021 PROFIMED. Все права защищены.</p>
+        </div>
+    </div>
+</footer>
